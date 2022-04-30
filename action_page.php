a6d6e056c0f7abe3728c9639c1ea5478ec2876c1
Md. Tangimul haque <72943561+Tanjim-joy@users.noreply.github.com>
2022-04-30 16:47:56 +0600

    portfolio
    My Simply portfolio

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Name <?php echo $_POST["name"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+Project: <?php echo $_POST["project"]; ?><br>
+Message: <?php echo $_POST["message"]; ?>
+</body>
+</html>
